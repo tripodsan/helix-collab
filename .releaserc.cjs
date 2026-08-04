fbdd2cec54fcf4c65448ad72f717b719ebfd4c65
Tobias Bocanegra <tripod@adobe.com>
2026-08-04 09:09:19 +0200

    chore: remove coralogix plugin

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -12,9 +12,6 @@ module.exports = {
       prepareCmd: 'npm run deploy && npm run test-postdeploy',
       publishCmd: 'npm run deploy-routes'
     }],
-    ['@adobe/semantic-release-coralogix', {
-      iconUrl: 'https://www.aem.live/media_13916754ab1f54a7a0b88dcb62cf6902d58148b1c.png',
-    }],
     ['@semantic-release/git', {
       assets: ['package.json', 'package-lock.json', 'CHANGELOG.md'],
       message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
